a46dfc3e45f00321adf2906ca348c92577bd2b90
UnSi <unsi31@gmail.com>
2020-10-22 19:32:58 +0300

    added serializers, movie list view, movie detail view

---
+++ movies/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import Movie
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+    """Список фильмов"""
+
+    class Meta:
+        model = Movie
+        fields = ('title', 'tagline', 'category')
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    """Фильм"""
+
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+
+
+    class Meta:
+        model = Movie
+        exclude = ('draft', )
+        fields = ('title', 'tagline', 'category')
