fff798811a145109f5f5763f93103d0558c32b1f
UnSi <unsi31@gmail.com>
2020-10-27 16:44:25 +0200

    get_client_ip was moved in separate file, added displaying avg rating, user rating

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ https://www.youtube.com/playlist?list=PLF-NY6ldwAWqSxUpnTBObEP21cFQxNJ7C
 - [x] Добавление и вывод отзывов к фильму - урок 4
 - [x] DRF рекурсивный вывод вложенных записей // вывод отзывов к фильму- урок 5
 - [x] Django Rest Framework - Добавление и обновление рейтинга к фильму - урок 6
-- [ ] Django Rest Framework - вывод рейтинга к фильмам - урок 7
+- [x] Django Rest Framework - вывод рейтинга к фильмам - урок 7
 - [ ] Django Rest Framework - ListAPIView и RetrieveAPIView, вывод актеров и режиссеров - урок 8
 - [ ] Django Rest Framework использование generics классов - урок 9
 - [ ] django-filter фильтрация фильмов по годам и жанрам - Django Rest Framework урок 10
--- movies/views.py
+++ movies/views.py
@@ -1,20 +1,32 @@
+from django.db import models
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer, ReviewSerializer, CreateRatingSerializer
+from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer, CreateRatingSerializer
+from .service import get_client_ip
 
 
 class MovieListView(APIView):
     """Вывод списка фильмов"""
+
     def get(self, request):
-        movies = Movie.objects.filter(draft=False)
+        movies = Movie.objects.filter(draft=False).annotate(
+            rating_user=models.Count("ratings", filter=models.Q(ratings__ip=get_client_ip(request)))  # models.Case(
+            # models.When(ratings__ip=get_client_ip(request), then=True),
+            # default=False,
+            # output_field=models.BooleanField()
+            # )
+        ).annotate(
+            middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+        )
         serializer = MovieListSerializer(movies, many=True)
         return Response(serializer.data)
 
 
 class MovieDetailView(APIView):
     """Вывод фильма"""
+
     def get(self, request, pk):
         movie = Movie.objects.get(id=pk, draft=False)
         serializer = MovieDetailSerializer(movie)
@@ -23,6 +35,7 @@ class MovieDetailView(APIView):
 
 class ReviewCreateView(APIView):
     """Добавление отзыва"""
+
     def post(self, request):
         review = ReviewCreateSerializer(data=request.data)
         if review.is_valid():
@@ -33,18 +46,10 @@ class ReviewCreateView(APIView):
 class AddStarRatingView(APIView):
     """Добавление рейтинга"""
 
-    def get_client_ip(self, request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
-        else:
-            ip = request.META.get('REMOTE_ADDR')
-        return ip
-
     def post(self, request):
         serializer = CreateRatingSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(ip=self.get_client_ip(request))
+            serializer.save(ip=get_client_ip(request))
             return Response(status=201)
         else:
             return Response(status=400)
