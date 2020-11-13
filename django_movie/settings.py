f213c80f639863a597a6a0f7abe7ae3040e4d680
UnSi <unsi31@gmail.com>
2020-11-13 16:54:49 +0200

    added mail confirmation

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -212,6 +212,7 @@ CKEDITOR_CONFIGS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
@@ -219,6 +220,26 @@ REST_FRAMEWORK = {
     ),
 }
 
+
+#SMTP
+try:
+    from .smtp import *
+except ImportError:
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = 'somemail@gmail.com'
+    EMAIL_HOST_PASSWORD = 'password'
+    EMAIL_PORT = 587
+
+
+
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+    'SERIALIZERS': {},
+}
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
